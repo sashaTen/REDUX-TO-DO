0b75d71e2c1ad195ed7e569205afc1157d11a2ae
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-06-15 10:15:58 +0900

    understood the destructuring

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
 import "./styles.css";
 import { useState } from "react";
 export default function App() {
-  const [count, setCount] = useState(0);
-  const [desiredNumber, setDesiredNumber] = useState(3);
-  const createNumber = (e) => {
-    e.preventDefault();
+  ///
 
-    setDesiredNumber(parseInt(e.target.value));
-  };
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <h2
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </h2>
-      <h2
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        +
-      </h2>
-      <h2
-        onClick={() => {
-          setCount(count + desiredNumber);
-        }}
-      >
-        + number : {desiredNumber}
-      </h2>
-      <input onChange={createNumber} />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
