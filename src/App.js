a5784bc6f2e96013e5deb22d52a36fd9548bbe83
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-06-15 11:47:51 +0900

    created the  small  app  using the redux and the state

--- src/App.js
+++ src/App.js
@@ -2,42 +2,6 @@ import "./styles.css";
 import { useState } from "react";
 import { configureStore } from "@reduxjs/toolkit";
 export default function App() {
-  ///
-
-  const original = {
-    value: 0,
-    job: "software  engineer"
-  };
-
-  const theReducer = (state = original, action) => {
-    if (action.type === "plus") {
-      return { ...state, value: state.value + 100 };
-    } else if (action.type === "changeJob") {
-      return { ...state, job: "web developer" };
-    }
-    return state;
-  };
-
-  const action = {
-    type: "plus"
-  };
-
-  const store = configureStore({
-    reducer: theReducer
-  });
-
-  store.dispatch({ type: "plus" });
-  store.dispatch({ type: "changeJob" });
-
-  //console.log(store.getState());
-
-  ////////  the  selectors
-
-  const selectValue = (state) => state.value;
-  const selectJob = (state) => state.job;
-  console.log(selectValue(store.getState()));
-  console.log(selectJob(store.getState()));
-
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
