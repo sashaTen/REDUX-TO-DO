9146dd5c4569c0b980ab1908c5faa98d2aba0387
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-06-16 11:32:04 +0900

    just  not  finished the slice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,82 @@
+/*
+
+carInfo  :  {
+  price  ,  name   ,   year
+}
+
+slice  create 
+slice.reducer put  into  the  store
+
+*/
+
 import "./styles.css";
-import { useState } from "react";
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { createContext, useState } from "react";
+import { combineReducers, createSlice, configureStore } from "@reduxjs/toolkit";
 export default function App() {
+  const original = {
+    value: 0
+  };
+  const countReducer = (state = original, action) => {
+    if (action.type === "plus") {
+      return state.value + 1000;
+    } else if (action.type === "minus") {
+      return state.value - 1000;
+    }
+
+    return state;
+  };
+
+  const action = (text) => {
+    return {
+      type: text
+    };
+  };
+  ///////////////////car   info
+
+  const carInfo = createSlice({
+    name: "carSlice",
+    initialState: {
+      year: 2020,
+      price: 20000,
+      carName: "mercedez"
+    },
+    reducers: {
+      deleteCarName: (state) => {
+        state.carName = "";
+      }
+    },
+    nullPrice: (state) => {
+      return { ...state, price: 0 };
+    }
+  });
+
+  /////////////change  name  reudcer  second  one
+  const user = {
+    name: "alecander",
+    age: 28,
+    job: "data   scientist"
+  };
+  const NameReducer = (state = user, action) => {
+    if (action.type === "uppercase") {
+      return { ...state, name: "Sasha Ten" };
+    }
+
+    return state;
+  };
+
+  const store = configureStore({
+    reducer: {
+      counter: countReducer,
+      changeName: NameReducer,
+      cars: carInfo.reducer
+    }
+  });
+
+  store.dispatch(action("carSlice/nullPrice"));
+  store.dispatch(action("uppercase"));
+  store.dispatch(action("minus"));
+  console.log(store.getState());
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
