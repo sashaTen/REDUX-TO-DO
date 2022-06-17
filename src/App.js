d81c8d59f4595577dd0b2027cc411c048b6417ed
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-06-17 18:29:41 +0900

    moved  the logic  to the redux  file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Redux from "./redux";
 import "./styles.css";
+import { useSelector, useDispatch } from "react-redux";
 import { createContext, useState } from "react";
 import { combineReducers, createSlice, configureStore } from "@reduxjs/toolkit";
 export default function App() {
   return (
     <div className="App">
       <Redux />
+
+      <button>-</button>
+      <h2></h2>
     </div>
   );
 }
--- src/redux.js
+++ src/redux.js
@@ -1,4 +1,42 @@
+import { createContext, startTransition, useState } from "react";
+import { combineReducers, createSlice, configureStore } from "@reduxjs/toolkit";
+
 const Redux = () => {
+  const counterSlice = createSlice({
+    name: "counter",
+    initialState: {
+      value: 0
+    },
+    reducers: {
+      increment: (state) => {
+        // Redux Toolkit allows us to write "mutating" logic in reducers. It
+        // doesn't actually mutate the state because it uses the immer library,
+        // which detects changes to a "draft state" and produces a brand new
+        // immutable state based off those changes
+        state.value += 1;
+      },
+      decrement: (state) => {
+        state.value -= 1;
+      },
+      incrementByAmount: (state, action) => {
+        state.value += action.payload;
+      }
+    }
+  });
+
+  const { increment, decrement } = counterSlice.actions;
+
+  const store = configureStore({
+    reducer: counterSlice.reducer
+  });
+  store.dispatch(increment());
+  store.dispatch(decrement());
+  store.dispatch(decrement());
+  store.dispatch(decrement());
+  store.dispatch(decrement());
+  store.dispatch(decrement());
+  console.log(store.getState());
+
   return <>hello redux</>;
 };
 
